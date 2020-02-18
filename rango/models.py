a6e4ffa9783a3f2dc96476cb88b9cfa2127a1f6d
He Wan <2470967w@student.gla.ac.uk>
2020-02-18 23:35:21 +0000

    database init

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from email.policy import default
 
-# Create your models here.
+class Catergory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    #primary key (model id, name)
+    
+    def __str__(self):
+        return self.name
+    #useful when debugging or accessing the object in Django shell: <Category: Python>
+    #override toString() in Java
+    
+class Page(models.Model):
+    category = models.ForeignKey(Catergory, on_delete=models.CASCADE)
+    # one-to-many relationship, field's constructor
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
+    
