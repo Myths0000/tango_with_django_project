4f1da31b2e9265eaf51ceb34a4e07284ad143e34
He Wan <2470967w@student.gla.ac.uk>
2020-02-19 19:40:14 +0000

    Chapter 7 OK

---
+++ rango/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from rango.models import Page, Category
+
+
+# inheriting from ModelForm
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+    # form widgets = text field / date picker
+    # HiddenInput()
+    
+    class Meta:
+        # Provide an association
+        # between the ModelForm and a model
+        # IMPORTANT !!
+        model = Category
+        fields = ('name',)
+        
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    
+    class Meta:
+        model = Page
+        # we are hiding the foreign key   
+        # why??
+        exclude = ('category',)
+
+    def clean(self):
+        # cleaned_data dictionary: stores all entered and processed data
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        
+        return cleaned_data # must always returning the reference
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
     # "-" = descending order
@@ -37,3 +38,73 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    
+    # check if the HTTP request was a POST (did the user submit data via the form)
+    # when submitting the contents of a HTML form
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        
+        if form.is_valid():
+            form.save(commit=True)
+            #cat = form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+            
+    return render(request, 'rango/add_category.html', {'form':form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+        
+    if category is None:
+        return redirect('/rango/')
+    
+    form = PageForm()
+    
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                
+                return redirect(reverse('rango:show_category', kwargs={'category_name_slug':category_name_slug}))
+            
+        else:
+            print(form.errors)
+
+    # passes the category object through to the template
+    context_dict = {'form': form, 'category': category} 
+    return render(request, 'rango/add_page.html', context=context_dict)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
